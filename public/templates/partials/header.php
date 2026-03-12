898d527d82b5d60fa2454f69da3ba6e37482ebe1
Livia <livia.kelebercova@gmail.com>
2026-03-12 13:51:51 +0100

    header fix

--- public/templates/partials/header.php
+++ public/templates/partials/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $app->getConfig("title"); ?></title>
+	<title>TechBlog</title>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
